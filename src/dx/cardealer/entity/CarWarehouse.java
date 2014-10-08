feac17603a89ee541832911f665b008069ab4c2d
JeffRabe <7livesleft@gmail.com>
2014-10-08 16:09:26 -0400

    Added new methods to CarWarehouse class for bulk imports, and added constructors.  Minor update to Customer class.

--- src/dx/cardealer/entity/CarWarehouse.java
+++ src/dx/cardealer/entity/CarWarehouse.java
@@ -2,6 +2,8 @@ package dx.cardealer.entity;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
 
 import dx.cardealer.entity.cars.Car;
 import dx.cardealer.entity.cars.CarMake;
@@ -10,10 +12,83 @@ import dx.cardealer.error.WarehouseFullException;
 
 public class CarWarehouse {
 	
-	private HashMap<CarSize, Integer> warehouseSpaces;
+	private HashMap<CarSize, Integer> warehouseSpacesUsed;
 	private HashMap<CarSize, Integer> warehouseCapacity;
 	private HashMap<CarMake, ArrayList<Car>> warehouseInventory;
 	
+	
+	public CarWarehouse( HashMap<CarSize, Integer> initialCapacities ){
+		
+		warehouseCapacity = new HashMap<CarSize, Integer>();
+		
+		for( CarSize size : CarSize.values() ){
+			if( !initialCapacities.containsKey(size) ){
+				throw new IllegalArgumentException(
+						"The following car size was not represented: " + size );
+			}
+			
+			int sizeCapacity = initialCapacities.get(size);
+			warehouseCapacity.put(size, sizeCapacity);
+		}
+		
+		warehouseSpacesUsed = new HashMap<CarSize, Integer>();
+		warehouseInventory = new HashMap<CarMake, ArrayList<Car>>();
+		for( CarMake make : CarMake.values() ){
+			warehouseInventory.put( make, new ArrayList<Car>() );
+		}
+		
+	}
+	
+	public CarWarehouse( HashMap<CarSize, Integer> initialCapacities,
+													List<Car> inventory )
+	throws WarehouseFullException
+	{
+		this( initialCapacities );
+		addNewCarInventory( inventory );
+	
+	}
+	
+	public void addNewCarInventory( List<Car> newInventory )
+	throws WarehouseFullException
+	{
+		if( !hasCapacityForCars(newInventory) ){
+			throw new WarehouseFullException(
+					"The warehouse does not have available space "
+					+ "for all of these cars."
+					);
+		}
+		
+		Iterator<Car> carItr = newInventory.iterator();
+		
+		while( carItr.hasNext() ){
+			Car newCar = carItr.next();
+			addCar( newCar );
+		}
+	}
+	
+	public boolean hasCapacityForCars( List<Car> newInventory ){
+		Iterator<Car> carItr = newInventory.iterator();
+		HashMap<CarSize, Integer> newInventoryTotals = new HashMap<CarSize, Integer>();
+		
+		for( CarSize size : CarSize.values() )
+			newInventoryTotals.put(size, 0);
+		
+		while( carItr.hasNext() ){
+			Car currentCar = carItr.next();
+			int sizeTotal = newInventoryTotals.get( currentCar.getSize() );
+			newInventoryTotals.put(currentCar.getSize(), ++sizeTotal );
+		}
+		
+		for( CarSize size : newInventoryTotals.keySet() ){
+			int newCarTotal = newInventoryTotals.get(size);
+			int availableSpaces = getAvailableSpaces(size);
+			
+			if( newCarTotal > availableSpaces )
+				return false;
+		}
+		
+		return true;
+	}
 	
 	public void addCar( Car newCar )
 	throws WarehouseFullException
@@ -24,9 +99,15 @@ public class CarWarehouse {
 		
 		ArrayList<Car> manufacturerCars = warehouseInventory.get(newCar.getMake());
 		manufacturerCars.add(newCar);
+		int spacesUsed = warehouseSpacesUsed.get(newCar.getSize());
+		warehouseSpacesUsed.put( newCar.getSize(), ++spacesUsed );
 	}
 	
 	public boolean spaceAvailable( CarSize size ){
-		return warehouseCapacity.get(size) > warehouseSpaces.get(size);
+		return getAvailableSpaces(size) > 0;
+	}
+	
+	public int getAvailableSpaces( CarSize size ){
+		return warehouseCapacity.get(size) - warehouseSpacesUsed.get(size);
 	}
 }
--- src/dx/cardealer/entity/Customer.java
+++ src/dx/cardealer/entity/Customer.java
@@ -7,4 +7,17 @@ public class Customer {
 	private String occupation;
 	private int annualIncome;
 	
+	public Customer( String firstName, String lastName, 
+									String occupation, int income ){
+		
+		if( income < 0 ){
+			throw new IllegalArgumentException(
+					"Annual Income must be 0 or greater: " + income );
+			
+		}
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.occupation = occupation;
+		this.annualIncome = income;
+	}
 }
